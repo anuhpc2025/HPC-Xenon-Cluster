7d33c63ac44ec99ca911a1437ed0cdf6a156af59
Sithum Dissanayake <contact@sithumd.com>
2025-08-26 14:30:54 +1000

    [main] Large test 4

--- input/HPL/Sithum/job3/job1.sh
+++ input/HPL/Sithum/job3/job1.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=hpl-test       # Job name
 #SBATCH --nodes=1                 # Number of nodes
 #SBATCH --ntasks=8                # Total MPI tasks
-#SBATCH --ntasks-per-node=2       # MPI tasks per node
+#SBATCH --ntasks-per-node=1       # MPI tasks per node
 #SBATCH --cpus-per-task=16        # CPU cores per MPI task
 #SBATCH --time=01:00:00           # Time limit hh:mm:ss
 #SBATCH --output=hpl-%j.out       # Standard output file
