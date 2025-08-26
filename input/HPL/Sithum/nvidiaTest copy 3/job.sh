793de3f1551a905414eaab31622e48aae5350357
Sithum Dissanayake <contact@sithumd.com>
2025-08-26 16:31:52 +1000

    [main] nvidia test 10

--- input/HPL/Sithum/nvidiaTest copy 3/job.sh
+++ input/HPL/Sithum/nvidiaTest copy 3/job.sh
@@ -7,16 +7,18 @@
 #SBATCH --time=01:00:00           # Time limit hh:mm:ss
 #SBATCH --output=hpl-%j.out       # Standard output file
 #SBATCH --error=hpl-%j.err        # Standard error file
+#SBATCH --nodelist=node1
 
 # Load MPI module (adjust for your system)
 # module load openmpi
 export LD_LIBRARY_PATH=/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib/nvshmem/:$LD_LIBRARY_PATH
 
 #testing
-export NVSHMEM_BOOTSTRAP_PMI=pmix
-export NVSHMEM_USE_IBGDA=0
 export NVSHMEM_USE_IBRC=0
+export NVSHMEM_USE_CUDA_IPC=1
 
+ulimit -l unlimited
+ulimit -n 65536
 
 # Run the MPI program
 mpirun ./xhpl
