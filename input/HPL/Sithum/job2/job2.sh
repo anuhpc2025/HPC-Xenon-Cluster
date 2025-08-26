81143049cf4221d2c08fb98cacebc7a5b9fb4cfa
Sithum Dissanayake <contact@sithumd.com>
2025-08-26 14:34:30 +1000

    [main] Large test 5

--- input/HPL/Sithum/job2/job2.sh
+++ input/HPL/Sithum/job2/job2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=hpl-test       # Job name
 #SBATCH --nodes=1                 # Number of nodes
-#SBATCH --ntasks=8                # Total MPI tasks
-#SBATCH --ntasks-per-node=1       # MPI tasks per node
-#SBATCH --cpus-per-task=16        # CPU cores per MPI task
+#SBATCH --ntasks=4                # Total MPI tasks
+#SBATCH --ntasks-per-node=4       # MPI tasks per node
+#SBATCH --cpus-per-task=8        # CPU cores per MPI task
 #SBATCH --time=01:00:00           # Time limit hh:mm:ss
 #SBATCH --output=hpl-%j.out       # Standard output file
 #SBATCH --error=hpl-%j.err        # Standard error file
