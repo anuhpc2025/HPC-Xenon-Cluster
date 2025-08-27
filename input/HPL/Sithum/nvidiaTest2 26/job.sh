6edc5af39f8b89684e0ef79c6ce73d42ec6aea41
Sithum Dissanayake <contact@sithumd.com>
2025-08-27 18:34:46 +1000

    [main] nvidia test 2 10

---
+++ input/HPL/Sithum/nvidiaTest2 26/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --ntasks=4                # Total MPI tasks
+#SBATCH --cpus-per-task=4         # CPU cores per MPI task
+#SBATCH --time=01:00:00           # Time limit hh:mm:ss
+#SBATCH --nodelist=node1
+
+# Load MPI module (adjust for your system)
+# module load openmpi
+export LD_LIBRARY_PATH=/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib/nvshmem/:$LD_LIBRARY_PATH
+
+#testing
+ulimit -l unlimited
+ulimit -n 65536
+
+# Run the MPI program
+mpirun bash -c 'echo "Rank $SLURM_PROCID on host $HOSTNAME, GPU: $CUDA_VISIBLE_DEVICES" && ./xhpl'
