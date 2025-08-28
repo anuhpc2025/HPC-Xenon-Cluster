bbccda2669e38a0712fda885dadd678a3b32adc4
Sithum Dissanayake <contact@sithumd.com>
2025-08-29 08:35:20 +1000

    [main] nvidia test 3 1

---
+++ input/HPL/Sithum/nvidiaTest3 4/job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --time=01:00:00           # Time limit hh:mm:ss
+#SBATCH --nodelist=node1
+
+# Load MPI module (adjust for your system)
+# module load openmpi
+export LD_LIBRARY_PATH=/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib/nvshmem/:$LD_LIBRARY_PATH
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export HPL_USE_GPU=1
+export HPL_CUDA_MODE=1
+
+#testing
+ulimit -l unlimited
+ulimit -n 65536
+
+# Run the MPI program
+mpirun -n 4 ./xhpl
