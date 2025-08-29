d38f9ad63bbd025f4d236658b796023c26fbd9ef
Daniel de Haan <u7623919@anu.edu.au>
2025-08-29 10:47:15 +1000

    job1

---
+++ input/HPL_NVIDIA/Daniel/job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --ntasks=4                # Total MPI tasks
+#SBATCH --cpus-per-task=4        # CPU cores per MPI task
+#SBATCH --time=01:00:00           # Time limit hh:mm:ss
+#SBATCH --nodelist=node1
+
+# Load MPI module (adjust for your system)
+# module load openmpi
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export HPL_USE_GPU=1
+export HPL_CUDA_MODE=1
+
+#testing
+ulimit -l unlimited
+ulimit -n 65536
+
+# Run the MPI program
+mpirun ./xhpl-nvidia
