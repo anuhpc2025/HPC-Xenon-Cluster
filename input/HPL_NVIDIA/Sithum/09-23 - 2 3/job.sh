fad331384dbb2abefbc305854ef5ffb7918dea3d
Sithum Dissanayake <u7080498@anu.edu.au>
2025-09-23 12:30:56 +1000

    [main] 09-23 2

---
+++ input/HPL_NVIDIA/Sithum/09-23 - 2 3/job.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --ntasks=2                # Total MPI tasks - 4 x 4
+#SBATCH --ntasks-per-node=1       # balance tasks across nodes - 1 per gpu
+#SBATCH --time=00:03:00           # Time limit hh:mm:ss
+#SBATCH --nodes=2                 #
+#SBATCH --nodelist=node3,node4    # nodes 1 and 2 are the only ones with hpcx for now
+
+# Load hpcx module - and everything else we need for that matter
+source ~/.bashrc
+
+# hpc-x
+export HPCX_HOME=/home/hpc/hpcx/hpcx-v2.24-gcc-doca_ofed-ubuntu24.04-cuda13-x86_64
+export LD_LIBRARY_PATH=$HPCX_HOME/ucx/lib:$HPCX_HOME/ompi/lib:$LD_LIBRARY_PATH
+export PATH=$HPCX_HOME/ompi/bin:$PATH
+source $HPCX_HOME/hpcx-init.sh
+hpcx_load
+
+# nvidia
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export HPL_USE_GPU=1
+export HPL_CUDA_MODE=1
+export LD_LIBRARY_PATH=/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib/omp:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64:/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib/nccl:/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/nvshmem/12:$LD_LIBRARY_PATH
+
+export OMPI_MCA_pml=ucx
+export OMPI_MCA_osc=ucx
+export OMPI_MCA_btl=^openib
+export OMPI_MCA_opal_cuda_support=true
+export OMPI_MCA_mpi_leave_pinned=1
+# Rank mapping/binding via MCA (equiv to --map-by ppr:2:numa:pe=8 --bind-to core)
+export OMPI_MCA_rmaps_base_mapping_policy="ppr:2:numa:pe=8"
+export OMPI_MCA_hwloc_base_binding_policy=core
+
+export UCX_TLS=rc_x,sm,self,cuda_copy,gdr_copy
+export UCX_IB_GPU_DIRECT_RDMA=y
+export UCX_MEMTYPE_CACHE=y
+export UCX_RNDV_SCHEME=put_zcopy
+export UCX_IB_PCI_RELAXED_ORDERING=on
+
+
+#testing
+ulimit -l unlimited
+ulimit -n 65536
+
+# Run the MPI program
+mpirun ./xhpl-nvidia
