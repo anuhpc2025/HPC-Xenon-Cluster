7059f9850a72e95434432b91450da073f8775150
Sithum Dissanayake <u7080498@anu.edu.au>
2025-09-23 11:44:51 +1000

    [main] 09-23 1 1

--- input/HPL_NVIDIA/Sithum/09-23 - 1 2/job.sh
+++ input/HPL_NVIDIA/Sithum/09-23 - 1 2/job.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=4       # balance tasks across nodes - 1 per gpu
 #SBATCH --time=01:00:00           # Time limit hh:mm:ss
 #SBATCH --nodes=2                 #
-#SBATCH --nodelist=node2,node4    # nodes 1 and 2 are the only ones with hpcx for now
+#SBATCH --nodelist=node1,node4    # nodes 1 and 2 are the only ones with hpcx for now
 
 # Load hpcx module - and everything else we need for that matter
 source ~/.bashrc
