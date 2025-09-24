f84f33d4cf366a03ae0e193ba9bd89ce703c401a
Sithum Dissanayake <contact@sithumd.com>
2025-09-24 17:54:49 +1000

    [main] 09-24 12 1

--- input/HPL_NVIDIA/Sithum/09-24 - 12 0/job.sh
+++ input/HPL_NVIDIA/Sithum/09-24 - 12 0/job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=hpl-test       # Job name
 #SBATCH --ntasks=16                # Total MPI tasks - 4 x 4
 #SBATCH --ntasks-per-node=4       # balance tasks across nodes - 1 per gpu
-#SBATCH --time=00:03:00           # Time limit hh:mm:ss
+#SBATCH --time=00:10:00           # Time limit hh:mm:ss
 #SBATCH --nodes=4                 #
 #SBATCH --nodelist=node1,node2,node3,node4    # nodes 1 and 2 are the only ones with hpcx for now
 
