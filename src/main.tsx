44143b297209de9ea9505f0751837bf829ca7ac5
Sithum Dissanayake <contact@sithumd.com>
2025-09-24 14:42:24 +1000

    [website] Fixing routes 5

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <HashRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
           <App />
       </HashRouter>,
   </StrictMode>,
