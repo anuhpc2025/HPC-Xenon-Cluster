d66821925790a7b60311e9ce3b77971bd83fba0e
Sithum Dissanayake <contact@sithumd.com>
2025-10-03 11:45:33 +1000

    [main] 10-03 - 1 2

--- input/HPL/Sithum/10-03 - 1/run.sh
+++ input/HPL/Sithum/10-03 - 1/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=hpl-test       # Job name
-#SBATCH --ntasks=32              # Total MPI tasks
+#SBATCH --ntasks=128              # Total MPI tasks
 #SBATCH --ntasks-per-node=32       # MPI tasks per node
 #SBATCH --cpus-per-task=1         # CPU cores per MPI task
 #SBATCH --time=00:20:00           # Time limit hh:mm:ss
 #SBATCH --output=hpl-%j.out       # Standard output file
 #SBATCH --error=hpl-%j.err        # Standard error file
-#SBATCH --nodes=1                 # Number of nodes
-#SBATCH --nodelist=node1    # nodes 1 and 2 are the only ones with hpcx for now
+#SBATCH --nodes=4                 # Number of nodes
+#SBATCH --nodelist=node1,node2,node3,node4    # nodes 1 and 2 are the only ones with hpcx for now
 
 # Load HPCX
 source ~/.bashrc
@@ -16,6 +16,17 @@ export LD_LIBRARY_PATH=$HPCX_HOME/ucx/lib:$HPCX_HOME/ompi/lib:$LD_LIBRARY_PATH
 export PATH=$HPCX_HOME/ompi/bin:$PATH
 source $HPCX_HOME/hpcx-init.sh
 hpcx_load
+
+# OMPI / UCX tuning
+export UCX_TLS=rc_x,sm,self,cuda_copy,gdr_copy,cuda_ipc
+export UCX_IB_GPU_DIRECT_RDMA=y
+export UCX_MEMTYPE_CACHE=y
+export UCX_RNDV_SCHEME=put_zcopy
+export UCX_IB_PCI_RELAXED_ORDERING=on
+
+# Ulimits
+ulimit -l unlimited
+ulimit -n 65536
 
 # Run the MPI program
 mpirun ./xhpl
