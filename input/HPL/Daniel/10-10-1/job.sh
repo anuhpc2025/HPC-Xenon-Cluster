e0e6eadf6c97e6e0ca99615c3306be2859717a76
Daniel de Haan <u7623919@anu.edu.au>
2025-10-10 09:31:42 +1100

    big HPL run

--- input/HPL/Daniel/10-10-1/job.sh
+++ input/HPL/Daniel/10-10-1/job.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=128              # Total MPI tasks
 #SBATCH --ntasks-per-node=32       # MPI tasks per node
 #SBATCH --cpus-per-task=1         # CPU cores per MPI task
-#SBATCH --time=10:00:00           # Time limit hh:mm:ss
+#SBATCH --time=99:00:00           # Time limit hh:mm:ss
 #SBATCH --nodes=4                 # Number of nodes
 #SBATCH --nodelist=node1,node2,node3,node4    # nodes 1 and 2 are the only ones with hpcx for now
 
