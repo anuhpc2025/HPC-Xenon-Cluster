bfe429ec1e4f7d008db532318892910d24873052
Daniel de Haan <u7623919@anu.edu.au>
2025-10-11 22:16:43 +1100

    overnight test

--- input/HPL/Daniel/11-10-1/run.sh
+++ input/HPL/Daniel/11-10-1/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=128              # Total MPI tasks
 #SBATCH --ntasks-per-node=32       # MPI tasks per node
 #SBATCH --cpus-per-task=1         # CPU cores per MPI task
-#SBATCH --time=99:00:00           # Time limit hh:mm:ss
+#SBATCH --time=10:00:00           # Time limit hh:mm:ss
 #SBATCH --nodes=4                 # Number of nodes
 #SBATCH --nodelist=node1,node2,node3,node4    # nodes 1 and 2 are the only ones with hpcx for now
 
@@ -28,7 +28,7 @@ export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64:/opt/nvidia/nvidia_hpc_benchma
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/nvshmem/12:$LD_LIBRARY_PATH
 
 # OMPI / UCX tuning
-export UCX_TLS=rc_x, sm,self, rc_mlx5, dc_x, ib, mm, posix, rc
+export UCX_TLS=rc_x,sm,self,cuda_copy,gdr_copy,cuda_ipc
 export UCX_IB_GPU_DIRECT_RDMA=y
 export UCX_MEMTYPE_CACHE=y
 export UCX_RNDV_SCHEME=put_zcopy
