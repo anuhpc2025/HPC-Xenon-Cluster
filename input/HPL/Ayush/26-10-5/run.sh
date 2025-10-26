7109261558fc284eaf0ad70378d2f6e7f41ee544
Xeno-Tek <u7508601@anu.edu.au>
2025-10-26 17:21:09 +1100

    Try run again

---
+++ input/HPL/Ayush/26-10-5/run.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --ntasks=128              # Total MPI tasks
+#SBATCH --ntasks-per-node=32       # MPI tasks per node
+#SBATCH --cpus-per-task=1         # CPU cores per MPI task
+#SBATCH --time=12:00:00           # Time limit hh:mm:ss
+#SBATCH --nodes=4                 # Number of nodes
+#SBATCH --nodelist=node1,node2,node3,node4    # nodes 1 and 2 are the only ones with hpcx for now
+
+# Load MPI module (adjust for your system)
+# module load openmpi
+
+# Load HPCX
+source ~/.bashrc
+export HPCX_ROOT=/home/hpc/hpcx/hpcx-v2.24-gcc-doca_ofed-ubuntu24.04-cuda13-x86_64
+source ${HPCX_ROOT}/hpcx-init.sh
+hpcx_load
+
+export PATH=${HPCX_ROOT}/ucx/bin:${HPCX_ROOT}/ompi/bin:${PATH}
+export LD_LIBRARY_PATH=${HPCX_ROOT}/ucx/lib:${HPCX_ROOT}/ompi/lib:${LD_LIBRARY_PATH}
+
+# === AOCL BLIS ===
+export AOCLROOT=/opt/AMD/aocl-5.1.0/5.1.0/gcc
+export LD_LIBRARY_PATH=${AOCLROOT}/lib:${LD_LIBRARY_PATH}
+
+export OMP_NUM_THREADS=1
+export OMP_PROC_BIND=true
+export OMP_PLACES=cores
+export BLIS_ENABLE_OPENMP=1
+export BLIS_CPU_EXT=ZEN3        
+export BLIS_DYNAMIC_SCHED=0
+
+# Optional: add CUDA & other NVIDIA libs, but *not* /opt/.../lib/nccl
+export LD_LIBRARY_PATH=/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib/omp:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64:/opt/nvidia/nvidia_hpc_benchmarks_openmpi/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/nvshmem/12:$LD_LIBRARY_PATH
+
+
+# OMPI / UCX tuning
+export UCX_TLS=rc_x,sm,self
+export UCX_IB_GPU_DIRECT_RDMA=y
+export UCX_MEMTYPE_CACHE=y
+export UCX_RNDV_SCHEME=put_zcopy
+export UCX_NET_DEVICES=mlx5_0:1
+
+# kill the noisy warning:
+unset UCX_IB_PCI_RELAXED_ORDERING
+
+# Ulimits
+ulimit -l unlimited
+ulimit -n 65536
+sync
+
+# Run the MPI program
+mpirun \
+  --map-by ppr:32:node:PE=1 --rank-by core --bind-to core --report-bindings \
+  -x LD_LIBRARY_PATH -x PATH \
+  -x AOCLROOT -x OMP_NUM_THREADS -x OMP_PROC_BIND -x OMP_PLACES \
+  -x BLIS_ENABLE_OPENMP -x BLIS_CPU_EXT -x BLIS_DYNAMIC_SCHED \
+  -x OMPI_MCA_pml -x OMPI_MCA_osc -x OMPI_MCA_btl \
+  -x UCX_TLS -x UCX_NET_DEVICES \
+  ./xhpl
