821dc2fa2a5b09ba26fbabcadc5097e7fdd509d9
Xeno-Tek <u7508601@anu.edu.au>
2025-11-10 17:58:14 +1100

    Large run with Performance mode on

--- input/HPL/Ayush/10-11-3/run.sh
+++ input/HPL/Ayush/10-11-3/run.sh
@@ -73,3 +73,4 @@ mpirun \
   -x OMPI_MCA_coll_ucc_enable -x OMPI_MCA_coll_hcoll_enable -x OMPI_MCA_coll_sharp_enable -x OMPI_MCA_coll \
   -x UCX_TLS -x UCX_NET_DEVICES -x UCX_RNDV_SCHEME \
   ./xhpl
+  
