bcc1be0a74355456656af3fbe19e77cecfc4bd4c
Xeno-Tek <u7508601@anu.edu.au>
2025-11-13 10:14:38 +1100

    NBmin = 2, swapping thresholde = 16, N = 435840, NB = 240

---
+++ input/HPL/Ayush/Xenon-Hero-Run/run.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --ntasks=16               # Total MPI tasks
+#SBATCH --ntasks-per-node=4       # MPI tasks per node
+#SBATCH --cpus-per-task=8         # CPU cores per MPI task
+#SBATCH --time=12:00:00           # Time limit hh:mm:ss
+#SBATCH --nodes=4                 # Number of nodes
+#SBATCH --nodelist=node1,node2,node3,node4    
+#SBATCH --hint=nomultithread
+
+# Load HPCX
+source ~/.bashrc
+export HPCX_HOME=/home/hpc/hpcx/hpcx-v2.24-gcc-doca_ofed-ubuntu24.04-cuda13-x86_64
+export PATH=${HPCX_HOME}/ucx/bin:${HPCX_HOME}/ompi/bin:${PATH}
+export LD_LIBRARY_PATH=${HPCX_HOME}/ucx/lib:${HPCX_HOME}/ompi/lib:${LD_LIBRARY_PATH}
+source ${HPCX_HOME}/hpcx-init.sh
+hpcx_load
+
+# === AOCL BLIS ===
+export AOCLROOT=/opt/AMD/aocl-5.1.0/5.1.0/gcc
+export LD_LIBRARY_PATH=${AOCLROOT}/lib:$LD_LIBRARY_PATH
+export BLIS_ENABLE_OPENMP=1
+export BLIS_CPU_EXT=ZEN3        
+export BLIS_DYNAMIC_SCHED=0
+
+# --- OpenMP for HPL (MPI-only run) ---
+export OMP_NUM_THREADS=8
+export OMP_PROC_BIND=close
+export OMP_PLACES=cores
+
+export OMP_DYNAMIC=false
+export OMP_MAX_ACTIVE_LEVELS=1
+
+# ---- NCCL choice: use system 2.27.7 ----
+unset LD_PRELOAD
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
+
+# OMPI / UCX tuning
+export UCX_TLS=rc_x,sm,self
+export UCX_NET_DEVICES=mlx5_0:1
+export UCX_RNDV_SCHEME=put_zcopy
+
+export UCX_RNDV_THRESH=16384
+export UCX_ZCOPY_THRESH=16384
+
+export UCX_MAX_EAGER_LANES=2
+export UCX_MAX_RNDV_LANES=2
+
+export OMPI_MCA_pml=ucx
+export OMPI_MCA_osc=ucx
+export OMPI_MCA_btl=^vader,tcp,openib
+
+# --- Open MPI over UCX, no fancy collectives ---
+export OMPI_MCA_coll_ucc_enable=0
+export OMPI_MCA_coll_hcoll_enable=0
+export OMPI_MCA_coll_sharp_enable=0
+export OMPI_MCA_coll="^ucc,hcoll,sharp"
+
+#Numa binding locality
+export OMPI_MCA_hwloc_base_binding_policy=core
+export OMPI_MCA_hwloc_base_mem_bind_policy=local_only
+
+# Ulimits
+ulimit -l unlimited
+ulimit -n 65536
+sync
+
+# Run the MPI program
+mpirun --rankfile rankfile.txt \
+  --report-bindings \
+  -x PATH -x LD_LIBRARY_PATH \
+  -x AOCLROOT -x OMP_NUM_THREADS -x OMP_PROC_BIND -x OMP_PLACES \
+  -x BLIS_ENABLE_OPENMP -x BLIS_CPU_EXT -x BLIS_DYNAMIC_SCHED \
+  -x OMPI_MCA_pml -x OMPI_MCA_osc -x OMPI_MCA_btl \
+  -x OMPI_MCA_coll_ucc_enable -x OMPI_MCA_coll_hcoll_enable -x OMPI_MCA_coll_sharp_enable -x OMPI_MCA_coll \
+  -x UCX_TLS -x UCX_NET_DEVICES -x UCX_RNDV_SCHEME \
+  ./xhpl
